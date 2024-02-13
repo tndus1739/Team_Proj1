a1a6d723ed98bde92c9f51855fdc922d989b4643
Sooyeon Noh <tndus1739@gmail.com>
2024-02-13 12:14:38 +0900

    초기세팅

---
+++ src/main/java/com/mysite/JpaQueryDSL/TeamProj1Application.java
@@ -0,0 +1,13 @@
+package com.mysite.JpaQueryDSL;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamProj1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamProj1Application.class, args);
+	}
+
+}
